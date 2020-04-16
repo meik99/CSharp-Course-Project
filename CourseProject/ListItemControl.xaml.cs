0ae4225a21d83c8a006c0029fdb198fd7dc3bb67
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2020-04-16 22:21:31 +0200

    Added missing classes

---
+++ CourseProject/ListItemControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using CourseProject.Common.Contract;
+using CourseProject.Common.Factory;
+using CourseProject.Database.Repository.Facade;
+
+namespace CourseProject
+{
+    public partial class ListItemControl : UserControl
+    {
+        ListItemViewModel _viewModel = new ListItemViewModel();
+
+        public ListItemControl()
+        {
+            InitializeComponent();
+            DataContext = _viewModel;
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            
+            UpdateItems();
+        }
+
+        private void UpdateItems()
+        {
+            var task = new ItemFacade().FindAll();
+            task.ContinueWith((result) => _viewModel.Items = result.Result);
+            task.Start();
+        }
+        
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Navigator.Instance.Navigate(new ManageItemControl());
+        }
+    }
+}
---
+++ CourseProject/ManageItemControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using CourseProject.Common.Builder;
+using CourseProject.Database.Repository.Facade;
+
+namespace CourseProject
+{
+    public partial class ManageItemControl : UserControl
+    {
+        ManageItemViewModel ManageItemViewModel { get; set; }
+        
+        public ManageItemControl()
+        {
+            InitializeComponent();
+            ManageItemViewModel = new ManageItemViewModel();
+            this.DataContext = ManageItemViewModel;
+        }
+
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            ManageItemViewModel.Amount = "0";
+            ManageItemViewModel.Description = string.Empty;
+            Navigator.Instance.Navigate(new ListItemControl());
+        }
+
+        private void OnAddClick(object sender, RoutedEventArgs e)
+        {
+            var task = new ItemFacade()
+                .Insert(new ItemBuilder()
+                    .SetDescription(ManageItemViewModel.Description)
+                    .SetAmount(Double.Parse(ManageItemViewModel.Amount))
+                    .Build());
+            task.ContinueWith(resultTask =>
+            {
+                Dispatcher?.Invoke(() =>
+                {
+                    Navigator.Instance.Navigate(new ListItemControl());
+                });
+                
+            });
+            task.Start();
+        }
+    }
+}
