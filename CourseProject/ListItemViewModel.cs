0110385bf4fd51bbd2fa0a98ae6d3413d7e1b3a1
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2020-04-16 22:15:01 +0200

    Implemented Listing Item and Styling List

---
+++ CourseProject/ListItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CourseProject.Annotations;
+using CourseProject.Common.Contract;
+
+namespace CourseProject
+{
+    public class ListItemViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        private readonly List<IItem> _items = new List<IItem>();
+
+        public List<IItem> Items 
+        { 
+            get => _items;
+            set
+            {
+                if (value != null)
+                {
+                    _items.Clear();
+                    _items.AddRange(value);
+                    OnPropertyChanged(nameof(Items));
+                }
+            }
+        }    
+    }
+}
